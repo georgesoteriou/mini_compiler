0b440cbbb80a1f9c7ed74f822d8fc3973db4723a
gs2617 <gs2617@ic.ac.uk>
2019-02-04 17:49:00 +0000

    implemented basic visitor

--- src/main/kotlin/uk/ac/ic/doc/wacc/Main.kt
+++ src/main/kotlin/uk/ac/ic/doc/wacc/Main.kt
@@ -2,13 +2,9 @@ package uk.ac.ic.doc.wacc
 
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
-import org.antlr.v4.runtime.tree.ErrorNode
-import org.antlr.v4.runtime.tree.ParseTree
-import org.antlr.v4.runtime.tree.RuleNode
-import org.antlr.v4.runtime.tree.TerminalNode
 import uk.ac.ic.doc.wacc.grammar.WaccLexer
 import uk.ac.ic.doc.wacc.grammar.WaccParser
-import uk.ac.ic.doc.wacc.grammar.WaccParserVisitor
+import uk.ac.ic.doc.wacc.grammar.WaccParserBaseVisitor
 
 fun main(args : Array<String>) {
 
@@ -22,98 +18,81 @@ fun main(args : Array<String>) {
 
 }
 
-class WaccVisitor: WaccParserVisitor<Void> {
+class WaccVisitor : WaccParserBaseVisitor<Void>() {
+
     override fun visitExpr(ctx: WaccParser.ExprContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitExpr(ctx)
     }
 
     override fun visitBinaryOper(ctx: WaccParser.BinaryOperContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitBinaryOper(ctx)
     }
 
     override fun visitUnaryOper(ctx: WaccParser.UnaryOperContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitUnaryOper(ctx)
     }
 
     override fun visitAssign_lhs(ctx: WaccParser.Assign_lhsContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitAssign_lhs(ctx)
     }
 
     override fun visitAssign_rhs(ctx: WaccParser.Assign_rhsContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitAssign_rhs(ctx)
     }
 
     override fun visitArg_list(ctx: WaccParser.Arg_listContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitArg_list(ctx)
     }
 
     override fun visitParam(ctx: WaccParser.ParamContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitParam(ctx)
     }
 
     override fun visitParam_list(ctx: WaccParser.Param_listContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitParam_list(ctx)
     }
 
     override fun visitStat(ctx: WaccParser.StatContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitStat(ctx)
     }
 
     override fun visitBase_type(ctx: WaccParser.Base_typeContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitBase_type(ctx)
     }
 
     override fun visitType(ctx: WaccParser.TypeContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitType(ctx)
     }
 
     override fun visitArray_elem(ctx: WaccParser.Array_elemContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitArray_elem(ctx)
     }
 
     override fun visitArray_type(ctx: WaccParser.Array_typeContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitArray_type(ctx)
     }
 
     override fun visitArray_liter(ctx: WaccParser.Array_literContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitArray_liter(ctx)
     }
 
     override fun visitPair_elem_type(ctx: WaccParser.Pair_elem_typeContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun visitPair_elem(ctx: WaccParser.Pair_elemContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun visitFunc(ctx: WaccParser.FuncContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun visitProg(ctx: WaccParser.ProgContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun visit(tree: ParseTree?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun visitErrorNode(node: ErrorNode?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun visitTerminal(node: TerminalNode?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitPair_elem_type(ctx)
     }
 
     override fun visitPair_type(ctx: WaccParser.Pair_typeContext?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return super.visitPair_type(ctx)
     }
 
-    override fun visitChildren(node: RuleNode?): Void {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun visitPair_elem(ctx: WaccParser.Pair_elemContext?): Void {
+        return super.visitPair_elem(ctx)
     }
 
+    override fun visitFunc(ctx: WaccParser.FuncContext?): Void {
+        return super.visitFunc(ctx)
+    }
 
+    override fun visitProg(ctx: WaccParser.ProgContext?): Void {
+        return super.visitProg(ctx)
+    }
 }
