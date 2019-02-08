a7c818c08d7f306a188c91fdee17d84e074ddcab
gs2617 <gs2617@ic.ac.uk>
2019-02-08 17:59:40 +0000

    added check for input file

--- src/main/kotlin/uk/ac/ic/doc/wacc/Main.kt
+++ src/main/kotlin/uk/ac/ic/doc/wacc/Main.kt
@@ -4,9 +4,13 @@ import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import uk.ac.ic.doc.wacc.grammar.WaccLexer
 import uk.ac.ic.doc.wacc.grammar.WaccParser
-import java.lang.Exception
 
 fun main(args : Array<String>) {
+
+    if(args.isEmpty()) {
+        println("Please enter a file")
+        return
+    }
 
     fun lexerForResource(resourceName: String) = WaccLexer(CharStreams.fromFileName(resourceName))
     fun tokenStream(resourceName: String) = CommonTokenStream(lexerForResource(resourceName))
