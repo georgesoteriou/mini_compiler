3c7843550de3e414e5f8e42fa2dc508fd38b46bb
nb1317 <nb1317@ic.ac.uk>
2019-02-11 17:09:35 +0000

    added return type property for functions

--- src/main/kotlin/uk/ac/ic/doc/wacc/ast/Function.kt
+++ src/main/kotlin/uk/ac/ic/doc/wacc/ast/Function.kt
@@ -1,3 +1,3 @@
 package uk.ac.ic.doc.wacc.ast
 
-data class Function(var name: Expression.Identifier, var params: Expression?, var block: Statement)
+data class Function(var name: Expression.Identifier, var params: Expression?, var block: Statement, var returnType: Type)
