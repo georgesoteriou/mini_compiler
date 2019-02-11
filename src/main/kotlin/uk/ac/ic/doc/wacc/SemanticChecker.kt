e9363f491d5b7f5f0d16f4b420b6c40e8ce4c7b5
nb1317 <nb1317@ic.ac.uk>
2019-02-11 16:40:09 +0000

    added while check

--- src/main/kotlin/uk/ac/ic/doc/wacc/SemanticChecker.kt
+++ src/main/kotlin/uk/ac/ic/doc/wacc/SemanticChecker.kt
@@ -1,8 +1,7 @@
 package uk.ac.ic.doc.wacc
 
+import uk.ac.ic.doc.wacc.ast.*
 import uk.ac.ic.doc.wacc.ast.Function
-import uk.ac.ic.doc.wacc.ast.Program
-import uk.ac.ic.doc.wacc.ast.Statement
 import uk.ac.ic.doc.wacc.visitors.ActiveScope
 
 fun semanticCheck (prog: Program) {
@@ -19,14 +18,81 @@ fun checkStatements(block: Statement.Block, activeScope: ActiveScope): Boolean {
     return valid
 }
 
+fun exprType(expr: Expression, activeScope: ActiveScope) : Type {
 
+
+    return Type.TError
+}
 
 fun checkStatement(param: Statement, activeScope: ActiveScope): Boolean {
 
     return when (param) {
         is Statement.Block -> checkStatements(param, activeScope)
 
+        is Statement.While -> {
+            var ret =  exprType(param.condition, activeScope) !is Type.TBool
+            ret = ret && checkStatements(param.then as Statement.Block, activeScope)
+            return ret
+        }
+        /*
 
+
+         */
+
+        is Statement.If -> true
+        /*
+
+
+         */
+
+        is Statement.PrintLn -> true
+        /*
+            evaluate expression recursively to see if:
+                expression has a uniform type
+                if no then problem
+                if yes then check if type is printable
+
+         */
+
+        is Statement.Print -> true
+        /*
+            same as above
+         */
+
+        is Statement.Exit -> true
+        /*
+
+         */
+
+        is Statement.Return -> true
+        /*
+            evaluate expression recursively to see if uniform type
+            if uniform type then see if returnable
+            if not uniform then problem
+
+         */
+
+        is Statement.FreeVariable -> true
+        /*
+
+
+         */
+
+        is Statement.ReadInput -> true
+        /*
+
+         */
+
+        is Statement.VariableAssignment -> true
+        /*
+
+            check if lhs var exists in scope
+            if it doesnt -> problem
+            if it does:
+                evaluate rhs to see what type
+                if types dont match -> problem
+                else : //todo
+         */
 
         is Statement.VariableDeclaration -> true
         /* check if lhs var exists in scope
@@ -38,7 +104,7 @@ fun checkStatement(param: Statement, activeScope: ActiveScope): Boolean {
                         add to activescope
          */
 
-        is Statement.VariableAssignment -> true
+
         else -> false
     }
 }
