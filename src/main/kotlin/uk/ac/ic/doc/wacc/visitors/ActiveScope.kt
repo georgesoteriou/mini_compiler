7211b6bc80d1cd8150ba37b2a6c10e26a319f871
gs2617 <gs2617@ic.ac.uk>
2019-02-13 14:00:02 +0000

    added duplicate variable definition errer

--- src/main/kotlin/uk/ac/ic/doc/wacc/visitors/ActiveScope.kt
+++ src/main/kotlin/uk/ac/ic/doc/wacc/visitors/ActiveScope.kt
@@ -9,15 +9,26 @@ class ActiveScope(private var currentScope: Scope, private var parentScope: Acti
     fun findDef(s: String): Optional<Type>
         = Optional.ofNullable(currentScope.definitions[s]).or { parentScope?.findDef(s) ?: Optional.empty() }
 
-    fun isVarInCurrScope(s: String): Boolean
-        = currentScope.definitions[s] != null
+    fun isVarInCurrScope(s: String): Boolean {
+        if(currentScope.definitions[s] != null) {
+            println("Error, $s already defined")
+            return true
+        }
+        return false
+    }
 
     fun add(def: Definition) {
         currentScope.definitions[def.name] = def.type
     }
 
-    fun addAll(defs: List<Definition>) {
-        defs.forEach { add(it) }
+    fun addAll(defs: List<Definition>): Boolean {
+        defs.forEach {
+            if(isVarInCurrScope(it.name)) {
+                return false
+            }
+            add(it)
+        }
+        return true
     }
 
     fun newSubScope(scope: Scope = Scope()): ActiveScope
