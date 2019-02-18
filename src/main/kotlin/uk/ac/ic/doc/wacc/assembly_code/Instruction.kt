ace323beebd21075e73f4274beae182af16c59a3
nbhat1998 <nbhat1998@gmail.com>
2019-02-18 14:52:30 +0000

    added some basic assembly instructions

--- src/main/kotlin/uk/ac/ic/doc/wacc/assembly_code/Instruction.kt
+++ src/main/kotlin/uk/ac/ic/doc/wacc/assembly_code/Instruction.kt
@@ -1,4 +1,10 @@
 package uk.ac.ic.doc.wacc.assembly_code
 
 sealed class Instruction {
+    data class LABEL(var name: String) : Instruction()
+    data class ADD(var rd: String, var rn: String, var operand: String): Instruction()
+    data class LDR(var rd: String, var addr_mode2: String): Instruction()
+    data class PUSH(var regList : List<String>): Instruction()
+    data class POP(var regList : List<String>): Instruction()
+    data class MOV(var rd: String, var rn: String): Instruction()
 }
