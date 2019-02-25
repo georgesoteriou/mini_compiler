e843f81ed7771f9bd414266711b598a78cf22025
nb1317 <nb1317@ic.ac.uk>
2019-02-25 17:56:10 +0000

    finished implemented DIV with the necessary extra instructions, and setting the required flags

--- src/main/kotlin/uk/ac/ic/doc/wacc/helpers/OperationHelpers.kt
+++ src/main/kotlin/uk/ac/ic/doc/wacc/helpers/OperationHelpers.kt
@@ -73,12 +73,14 @@ fun CodeGenerator.binOpInstructions(expr: Expression.BinaryOperation, dest: Int)
                 arrayListOf(
                     Instruction.MOV(Operand.Register(0), Operand.Register(dest)),
                     Instruction.MOV(Operand.Register(1), Operand.Register(dest + 1)),
-                    //TODO: BL p_check_divide_by_zero
+                    Instruction.BCond("p_check_divide_by_zero","L"),
                     Instruction.BL("__aeabi_idiv"),
                     Instruction.MOV(Operand.Register(dest),Operand.Register(0))
                     )
             )
-            //TODO: flags that need to be set : printCheckDivideByZero, printThrowRuntime, printString
+            divideByZeroFlag = true
+            throwRuntimeFlag = true
+            printStringFlag = true
         }
         Expression.BinaryOperator.MOD -> {
             instructions.addAll(
