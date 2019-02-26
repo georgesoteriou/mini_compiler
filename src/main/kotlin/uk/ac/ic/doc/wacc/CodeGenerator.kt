8872033eb492924df11fa86a729033a43354547b
Iulia Ivana <imi17@ic.ac.uk>
2019-02-26 13:45:34 +0000

    Fixed printing assembly to file

--- src/main/kotlin/uk/ac/ic/doc/wacc/CodeGenerator.kt
+++ src/main/kotlin/uk/ac/ic/doc/wacc/CodeGenerator.kt
@@ -25,7 +25,7 @@ class CodeGenerator(var program: Program) {
     var printLnTag = -1
     var freeArrayTag = -1
     var freePairTag = -1
-    var intInputTag = - 1
+    var intInputTag = -1
     var charInputTag = -1
     var throwOverflowTag = -1
     var divideByZeroTag = -1
@@ -51,6 +51,7 @@ class CodeGenerator(var program: Program) {
         //TODO: Add functions to active scope
 
         //TODO: function foreach loop
+        instructions.add(Instruction.LABEL("main"))
         compileStatement(program.block, "main")
         instructions.add(Instruction.Flag(".ltorg"))
 
@@ -66,7 +67,7 @@ class CodeGenerator(var program: Program) {
             printBoolTrueTag = messageCounter - 1
             messageTagGenerator("false\\0", 1)
             printBoolFalseTag = messageCounter - 1
-            add_pPrintBool(printBoolTrueTag,printBoolFalseTag)
+            add_pPrintBool(printBoolTrueTag, printBoolFalseTag)
         }
 
         if (printIntFlag) {
@@ -88,19 +89,22 @@ class CodeGenerator(var program: Program) {
         }
 
         if (intInputFlag) {
-            messageTagGenerator("%d\\0",1)
+            messageTagGenerator("%d\\0", 1)
             intInputTag = messageCounter - 1
             add_intInput(intInputTag)
         }
 
         if (charInputFlag) {
-            messageTagGenerator("%c\\0",1)
+            messageTagGenerator("%c\\0", 1)
             charInputTag = messageCounter - 1
             add_charInput(charInputTag)
         }
 
         if (throwOverflowFlag) {
-            messageTagGenerator("OverflowError: the result is too small/large to store in a 4-byte signed-integer.\\n",1)
+            messageTagGenerator(
+                "OverflowError: the result is too small/large to store in a 4-byte signed-integer.\\n",
+                1
+            )
             throwOverflowTag = messageCounter - 1
             add_throwOverflowError(throwOverflowTag)
         }
@@ -111,7 +115,7 @@ class CodeGenerator(var program: Program) {
         }
 
         if (divideByZeroFlag) {
-            messageTagGenerator("DivideByZeroError: divide or modulo by zero\\n\\0",2)
+            messageTagGenerator("DivideByZeroError: divide or modulo by zero\\n\\0", 2)
             divideByZeroTag = messageCounter - 1
             add_checkDivideByZero(divideByZeroTag)
         }
@@ -119,7 +123,7 @@ class CodeGenerator(var program: Program) {
         if (freeArrayFlag || freePairFlag) {
 
             if (freeArrayFlag) {
-                messageTagGenerator("NullReferenceError: dereference a null reference\\n\\0",2)
+                messageTagGenerator("NullReferenceError: dereference a null reference\\n\\0", 2)
                 freeArrayTag = messageCounter - 1
                 add_freeArray(freeArrayTag)
                 if (throwRuntimeFlag) {
@@ -128,7 +132,7 @@ class CodeGenerator(var program: Program) {
             }
 
             if (freePairFlag) {
-                messageTagGenerator("NullReferenceError: dereference a null reference\\n\\0",2)
+                messageTagGenerator("NullReferenceError: dereference a null reference\\n\\0", 2)
                 freePairTag = messageCounter - 1
                 add_freePair(freePairTag)
                 if (throwRuntimeFlag) {
@@ -146,28 +150,21 @@ class CodeGenerator(var program: Program) {
 
         val file = File("$filename.s")
 
-        val isCreated :Boolean = file.createNewFile()
-
-        if(isCreated){
-            println("$filename compiled successfully.")
-            if(!data.isEmpty()) {
-                file.writeText(".data")
-            }
-
-            data.forEach { file.appendText(it.toString() + "\n") }
-
-            instructions.forEach {  file.appendText(it.toString() + "\n") }
-        } else{
-            println("$filename already exists.")
+        if (!data.isEmpty()) {
+            file.writeText(".data\n")
         }
+
+        data.forEach { file.appendText(it.toString() + "\n") }
+
+        instructions.forEach { file.appendText(it.toString() + "\n") }
     }
 
     fun compileStatement(statement: Statement, name: String = ".L$labelCounter") {
         when (statement) {
             is Statement.Block -> {
-               statement.scope.findFullSize()
+                statement.scope.findFullSize()
                 //instructions.add(Instruction.LABEL(name))
-                if(name == "main") {
+                if (name == "main") {
                     instructions.add(Instruction.PUSH(arrayListOf(Operand.Lr)))
                 }
                 activeScope = activeScope.newSubScope(statement.scope)
@@ -177,7 +174,7 @@ class CodeGenerator(var program: Program) {
                 // TODO i don't think label counter should be handles here
                 increaseSP(statement)
 
-                if(name == "main") {
+                if (name == "main") {
                     instructions.add(Instruction.LDRSimple(Operand.Register(0), Operand.Literal.LInt("0")))
                     instructions.add(Instruction.POP(arrayListOf(Operand.Pc)))
                 }
@@ -187,15 +184,15 @@ class CodeGenerator(var program: Program) {
             is Statement.VariableDeclaration -> {
                 var type = statement.lhs.type
                 val name = statement.lhs.name
-                if(statement.rhs is Expression.Literal.LPair) {
+                if (statement.rhs is Expression.Literal.LPair) {
                     pairNullInstructions()
                 } else {
                     when (type) {
-                        is Type.TInt,  is Type.TString-> {
+                        is Type.TInt, is Type.TString -> {
                             compileExpression(statement.rhs, 4)
                             wordAssignInstructions(name)
                         }
-                        is Type.TBool, is Type.TChar  -> {
+                        is Type.TBool, is Type.TChar -> {
                             compileExpression(statement.rhs, 4)
                             byteAssignInstructions(name)
                         }
@@ -215,7 +212,7 @@ class CodeGenerator(var program: Program) {
                 var type = statement.rhs.exprType
                 val name = (statement.lhs as Expression.Identifier).name
                 when (type) {
-                    is Type.TInt, is Type.TString-> {
+                    is Type.TInt, is Type.TString -> {
                         compileExpression(statement.rhs, 4)
                         wordAssignInstructions(name)
                     }
@@ -224,19 +221,19 @@ class CodeGenerator(var program: Program) {
                         byteAssignInstructions(name)
                     }
                     is Type.TArray -> {
-                        val def = Definition(name,statement.lhs.exprType)
+                        val def = Definition(name, statement.lhs.exprType)
                         arrayAssignInstructions(def, statement.rhs as Expression.Literal.LArray)
                     }
                     is Type.TPair -> {
                         val def = Definition(name, statement.lhs.exprType)
-                        pairAssignInstructions(def,statement.rhs as Expression.Literal.LPair)
+                        pairAssignInstructions(def, statement.rhs as Expression.Literal.LPair)
                     }
                 }
             }
             is Statement.ReadInput -> {
                 //compileExpression(statement.expression,4)
-                instructions.add(Instruction.ADD(Operand.Register(4),Operand.Sp,Operand.Constant(0)))
-                instructions.add(Instruction.MOV(Operand.Register(0),Operand.Register(4)))
+                instructions.add(Instruction.ADD(Operand.Register(4), Operand.Sp, Operand.Constant(0)))
+                instructions.add(Instruction.MOV(Operand.Register(0), Operand.Register(4)))
 
                 when {
                     Type.compare(statement.expression.exprType, Type.TInt) -> {
@@ -244,24 +241,24 @@ class CodeGenerator(var program: Program) {
                         intInputFlag = true
                     }
 
-                    Type.compare(statement.expression.exprType,Type.TChar) -> {
+                    Type.compare(statement.expression.exprType, Type.TChar) -> {
                         instructions.add(Instruction.BL("p_read_char"))
                         charInputFlag = true
                     }
                 }
             }
             is Statement.FreeVariable -> {
-                compileExpression(statement.expression,4)
-                instructions.add(Instruction.MOV(Operand.Register(0),Operand.Register(4)))
+                compileExpression(statement.expression, 4)
+                instructions.add(Instruction.MOV(Operand.Register(0), Operand.Register(4)))
                 when {
-                    Type.compare(statement.expression.exprType,Type.TArray(Type.TAny)) -> {
+                    Type.compare(statement.expression.exprType, Type.TArray(Type.TAny)) -> {
                         printStringFlag = true
                         freeArrayFlag = true
                         throwRuntimeFlag = true
                         instructions.add(Instruction.BL("p_free_array"))
                     }
 
-                    Type.compare(statement.expression.exprType, Type.TPair(Type.TAny,Type.TAny)) -> {
+                    Type.compare(statement.expression.exprType, Type.TPair(Type.TAny, Type.TAny)) -> {
                         printStringFlag = true
                         freePairFlag = true
                         throwRuntimeFlag = true
@@ -360,7 +357,7 @@ class CodeGenerator(var program: Program) {
                 val e1 = expression.e1
                 val e2 = expression.e2
 
-                if(weight(expression.e1) >= weight(expression.e2)) {
+                if (weight(expression.e1) >= weight(expression.e2)) {
                     compileExpression(e1, dest)
                     compileExpression(e2, dest + 1)
                 } else {
