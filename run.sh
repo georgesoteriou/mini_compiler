91eda2f6478932a90c9de1edbadd4ae0c179f0e0
gs2617 <gs2617@ic.ac.uk>
2019-03-08 11:03:55 +0000

    fixed runing bug added in last commit

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 rm -f $1.s $1
 echo "======================================================="
 if ./compile $1.wacc; then
-    arm-linux-gnueabi-gcc -o $1 -mcpu=arm1176jzf-s -mtune=arm1176jzf-s $1.s
+    arm-linux-gnueabi-gcc -o $1.out -mcpu=arm1176jzf-s -mtune=arm1176jzf-s $1.s
     qemu-arm -L /usr/arm-linux-gnueabi/ $1.out
 else
     echo "FAILED TO COMPILE"
