ad69e70203747ddf0adbb38663e26a89dc1cbdfe
Iulia Ivana <imi17@ic.ac.uk>
2019-03-09 18:06:41 +0000

    added visitors for unary and binary side effecting expressions, ToDo: possibly consider merging later

--- src/main/kotlin/uk/ac/ic/doc/wacc/visitors/UnSideEffVisitor.kt
+++ src/main/kotlin/uk/ac/ic/doc/wacc/visitors/UnSideEffVisitor.kt
@@ -1,2 +1,14 @@
 package uk.ac.ic.doc.wacc.visitors
 
+import uk.ac.ic.doc.wacc.ast.Expression
+import uk.ac.ic.doc.wacc.grammar.WaccParser
+import uk.ac.ic.doc.wacc.grammar.WaccParserBaseVisitor
+
+class UnSideEffVisitor: WaccParserBaseVisitor<Expression.BinaryOperator>() {
+    override fun visitPlus_plus(ctx: WaccParser.Plus_plusContext): Expression.BinaryOperator
+        = Expression.BinaryOperator.PLUS
+
+
+    override fun visitMinus_minus(ctx: WaccParser.Minus_minusContext): Expression.BinaryOperator
+        = Expression.BinaryOperator.MINUS
+}
